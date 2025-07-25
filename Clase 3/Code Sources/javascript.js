734ae8e4a94bd64f290eadf565b9efcf37a248cd
jmanuel.gh <110192267+jmanueltorress@users.noreply.github.com>
2025-07-24 21:32:47 -0600

    add class 3

---
+++ Clase 3/Code Sources/javascript.js
@@ -0,0 +1,23 @@
+
+//Variables
+let nombre = "Juan";
+const edad = 25;
+let edadJuan= 25;
+var activo = true;
+
+
+/**
+ * Condicionales
+ */
+// if (edad >= 18) {
+//   console.log("Es mayor de edad");
+// } else {
+//   console.log("Es menor de edad");
+// }
+
+//Funciones
+function saludar(nombre) {
+  return "Hola, " + nombre;
+  
+}
+console.log(saludar(nombre));
